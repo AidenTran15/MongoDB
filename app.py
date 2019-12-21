b70092f758a31b5c9c81a480dbd3c2888a0dfe09
AidenTran15 <aidenkiettran@gmail.com>
2019-12-21 13:27:21 +1100

    [Update] Create app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from models.post import Post
 
-post = Post()
+post = Post("Post1 title", "Post1 content", "Post1 author")
+post2 = Post("Post2 title", "Post2 content", "Post2 author")
+
 print(post.author)
+print(post2.author)
